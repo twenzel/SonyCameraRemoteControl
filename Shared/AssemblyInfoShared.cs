aec9f326639c6a69ca998e95e3864369602e409a
twenzel <toni.wenzel@googlemail.com>
2015-06-14 19:17:34 +0200

    New assembly infos

---
+++ Shared/AssemblyInfoShared.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+
+[assembly: AssemblyCompany ("Created by Toni. https://github.com/twenzel/SonyCameraRemoteControl")]
+[assembly: AssemblyProduct ("Sony Camera Remote Library")]
+[assembly: AssemblyCopyright ("Released under the MIT license; https://github.com/twenzel/SonyCameraRemoteControl")]
+[assembly: AssemblyTrademark ("")]
+
+[assembly: AssemblyVersion ("1.0.0")]
+
+
--- SonyCamerRemoteControl.Portable/Properties/AssemblyInfo.cs
+++ SonyCamerRemoteControl.Portable/Properties/AssemblyInfo.cs
@@ -1,11 +1,12 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle ("SonyCameraRemoteControl.NetFx45")]
-[assembly: AssemblyDescription (".NET 4.5 specfic implementation of SonyCameraRemoteControl")]
+[assembly: AssemblyTitle ("Sony Camera Remote Control Library")]
+[assembly: AssemblyDescription ("Platform independent implementation for SCRC library.")]
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCulture ("")]
+
 
